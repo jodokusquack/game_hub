00c918539452bf5c52e992d85ccd81fde0ba0a1f
Jakob Blaschke <jakobblaschke@yahoo.de>
2021-09-17 13:45:21 +0200

    Enable live reload for HTML with webpacker-dev-server

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,13 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
+// Added to make webpack reload on html changes
+const chokidar = require('chokidar')
+
+environment.config.devServer.before = (app, server) => {
+    chokidar.watch([
+        'app/views/**/*.erb'
+    ]).on('change', () => server.sockWrite(server.sockets, 'content-changed'))
+}
 
 module.exports = environment.toWebpackConfig()
