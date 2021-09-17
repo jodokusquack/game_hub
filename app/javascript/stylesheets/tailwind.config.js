1473064396b65eada6e3cf7d74b26dd1c6fe46ab
Jakob Blaschke <jakobblaschke@yahoo.de>
2021-09-17 13:38:48 +0200

    Add TailwindCSS and configure it

---
+++ app/javascript/stylesheets/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    './app/**/*.html.erb', //I don't know yet what this does.
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
